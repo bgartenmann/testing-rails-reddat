246d3003d4c910c9de62c13653c82ca90b0890f6
Beni Gartenmann <b.gartenmann@gmail.com>
2017-04-01 21:36:32 +0200

    - implemented voting on links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :links, only: [:show, :new, :create]
+  resources :links, only: [:show, :new, :create] do
+    resource :upvote, only: [:create]
+    resource :downvote, only: [:create]
+  end
 
   root to: "links#index"
 end
---
+++ db/migrate/20170401191041_add_voting_columns_to_link.rb
@@ -0,0 +1,6 @@
+class AddVotingColumnsToLink < ActiveRecord::Migration[5.1]
+  def change
+    add_column :links, :upvotes, :integer, default: 0, null: false
+    add_column :links, :downvotes, :integer, default: 0, null: false
+  end
+end
