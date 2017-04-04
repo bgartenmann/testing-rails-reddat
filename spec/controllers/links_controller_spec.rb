d5de078bdd44b16922c34d981573409b09658a0e
Beni Gartenmann <b.gartenmann@gmail.com>
2017-04-04 10:41:44 +0200

    - rewrote links_controller_spec with doubles and stubbing

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -15,7 +15,10 @@ RSpec.describe LinksController, "#create" do
 
   context "when the link is invalid" do
     it "re-renders the form" do
-      post :create, params: {link: attributes_for(:link, :invalid)}
+      invalid_link = double(save: false)
+      allow(Link).to receive(:new).and_return(invalid_link)
+
+      post :create, params: {link: { attribute: "value" } }
 
       expect(response).to render_template :new
     end
